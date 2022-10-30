9fe96b83dc457cd57ff107bb470ce16b6ca3e90a
Sam Chen <sam8211112222@gmail.com>
2022-10-30 11:38:04 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello world")
+	})
+
+	log.Fatal(router.Run(":3000"))
+}
