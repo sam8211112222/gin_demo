bc87c4bb489ed9f93e994b3eac404d20bed67642
Sam Chen <sam8211112222@gmail.com>
2022-10-30 13:10:34 +0800

    gin serve static file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "embed"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
@@ -9,9 +10,11 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello world")
-	})
+	router.StaticFile("/", "./public/index.html") // http://localhost:3000/
+
+	router.Static("/public", "./public") // http://localhost:3000/public/employee.html
+
+	router.StaticFS("/fs", http.Dir("./public")) // http://localhost:3000/fs/employee.html
 
 	log.Fatal(router.Run(":3000"))
 }
