100d6b08c5327bbbaf6a981cf8de291857da57ed
Sam Chen <sam8211112222@gmail.com>
2022-10-31 13:31:50 +0800

    http request object

--- main.go
+++ main.go
@@ -5,20 +5,41 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 func main() {
 	router := gin.Default()
-	// go to http://localhost:3000/rest and find result on the page
-	router.GET("/rest", func(c *gin.Context) {
-		url := c.Request.URL.String()
-		headers := c.Request.Header
-		cookies := c.Request.Cookies()
-		c.IndentedJSON(http.StatusOK, gin.H{
-			"url":     url,
-			"headers": headers,
-			"cookies": cookies,
+
+	router.GET("/query/*rest", func(c *gin.Context) {
+		username := c.Query("username")
+		year := c.DefaultQuery("year", strconv.Itoa(time.Now().Year()))
+		months := c.QueryArray("month")
+
+		c.JSON(http.StatusOK, gin.H{
+			"username": username,
+			"year":     year,
+			"months":   months,
 		})
 	})
+
+	router.GET("/employee", func(c *gin.Context) {
+		c.File("./public/employee.html")
+	})
+
+	router.POST("/employee", func(c *gin.Context) {
+		date := c.PostForm("date")
+		amount := c.PostForm("amount")
+		username := c.DefaultPostForm("username", "adent")
+
+		c.IndentedJSON(http.StatusOK, gin.H{
+			"date":     date,
+			"amount":   amount,
+			"username": username,
+		})
+
+	})
+
 	log.Fatal(router.Run(":3000"))
 }
