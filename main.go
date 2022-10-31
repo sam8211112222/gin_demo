21bf48e8bd06cb4d6354c5228bf921b3aa75ac61
Sam Chen <sam8211112222@gmail.com>
2022-10-31 13:26:07 +0800

    http request object

--- main.go
+++ main.go
@@ -9,20 +9,16 @@ import (
 
 func main() {
 	router := gin.Default()
-	// http://localhost:3000/admin --> 404 page not found
-	adminGroup := router.Group("/admin")
-	// http://localhost:3000/admin/users
-	adminGroup.GET("/users", func(c *gin.Context) {
-		c.String(http.StatusOK, "page to admin users")
-	})
-	// http://localhost:3000/admin/roles
-	adminGroup.GET("/roles", func(c *gin.Context) {
-		c.String(http.StatusOK, "page to admin roles")
-	})
-
-	// http://localhost:3000/admin/polices
-	adminGroup.GET("/polices", func(c *gin.Context) {
-		c.String(http.StatusOK, "page to admin polices")
+	// go to http://localhost:3000/rest and find result on the page
+	router.GET("/rest", func(c *gin.Context) {
+		url := c.Request.URL.String()
+		headers := c.Request.Header
+		cookies := c.Request.Cookies()
+		c.IndentedJSON(http.StatusOK, gin.H{
+			"url":     url,
+			"headers": headers,
+			"cookies": cookies,
+		})
 	})
 	log.Fatal(router.Run(":3000"))
 }
