533c2ff0246703914733b3239144cc2eb2d6f585
Sam Chen <sam8211112222@gmail.com>
2022-10-31 13:56:29 +0800

    data binding

--- main.go
+++ main.go
@@ -5,42 +5,48 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
-	"strconv"
 	"time"
 )
 
+type TimeoffRequest struct {
+	Date   time.Time `json:"date" form:"date" time_format:"2006-01-02" binding:"-"`
+	Amount float64   `json:"amount" form:"amount" binding:"-"`
+}
+
 func main() {
 	router := gin.Default()
-
-	// url: http://localhost:3000/query/?username=sam&year=2022&month=1&month=2&month=3
-	router.GET("/query/*rest", func(c *gin.Context) {
-		username := c.Query("username")
-		year := c.DefaultQuery("year", strconv.Itoa(time.Now().Year()))
-		months := c.QueryArray("month")
-
-		c.JSON(http.StatusOK, gin.H{
-			"username": username,
-			"year":     year,
-			"months":   months,
-		})
-	})
 
 	router.GET("/employee", func(c *gin.Context) {
 		c.File("./public/employee.html")
 	})
 
-	// url: http://localhost:3000/employee, try choose a day and amount
 	router.POST("/employee", func(c *gin.Context) {
-		date := c.PostForm("date")
-		amount := c.PostForm("amount")
-		username := c.DefaultPostForm("username", "adent")
+		var timeoffRequest TimeoffRequest
 
-		c.IndentedJSON(http.StatusOK, gin.H{
-			"date":     date,
-			"amount":   amount,
-			"username": username,
-		})
+		// if binding success
+		if err := c.ShouldBind(&timeoffRequest); err == nil {
+			c.JSON(http.StatusOK, timeoffRequest)
+		} else {
+			c.String(http.StatusInternalServerError, err.Error())
+		}
+	})
 
+	apiGroup := router.Group("/api")
+
+	/*
+		Works with message:
+		{
+			"date": "2022-12-31T00:00:00Z",
+			"amount": 8
+		}
+	*/
+	apiGroup.POST("/timeoff", func(c *gin.Context) {
+		var timeoffRequest TimeoffRequest
+		if err := c.ShouldBindJSON(&timeoffRequest); err == nil {
+			c.JSON(http.StatusOK, timeoffRequest)
+		} else {
+			c.String(http.StatusInternalServerError, err.Error())
+		}
 	})
 
 	log.Fatal(router.Run(":3000"))
