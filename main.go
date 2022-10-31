0d1bb21bdca57c90578ba56c2dc9e889f04b11da
Sam Chen <sam8211112222@gmail.com>
2022-10-31 14:05:21 +0800

    data validation

--- main.go
+++ main.go
@@ -3,33 +3,32 @@ package main
 import (
 	_ "embed"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	"log"
 	"net/http"
 	"time"
 )
 
 type TimeoffRequest struct {
-	Date   time.Time `json:"date" form:"date" time_format:"2006-01-02" binding:"-"`
-	Amount float64   `json:"amount" form:"amount" binding:"-"`
+	Date   time.Time `json:"date" form:"date" time_format:"2006-01-02" binding:"required,future"`
+	Amount float64   `json:"amount" form:"amount" binding:"required,gt=0"`
+}
+
+var ValidatorFuture validator.Func = func(fl validator.FieldLevel) bool {
+	date, ok := fl.Field().Interface().(time.Time)
+	if ok {
+		return date.After(time.Now())
+	}
+	return true
 }
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/employee", func(c *gin.Context) {
-		c.File("./public/employee.html")
-	})
-
-	router.POST("/employee", func(c *gin.Context) {
-		var timeoffRequest TimeoffRequest
-
-		// if binding success
-		if err := c.ShouldBind(&timeoffRequest); err == nil {
-			c.JSON(http.StatusOK, timeoffRequest)
-		} else {
-			c.String(http.StatusInternalServerError, err.Error())
-		}
-	})
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("future", ValidatorFuture)
+	}
 
 	apiGroup := router.Group("/api")
 
@@ -37,6 +36,17 @@ func main() {
 		Works with message:
 		{
 			"date": "2022-12-31T00:00:00Z",
+			"amount": 8
+		}
+
+		Fails with messages:
+		{
+			"date": "2022-12-31T00:00:00Z",
+			"amount": -4											// invalid amount
+		}
+
+		{
+			"date": "2000-01-01T00:00:00Z",  	// invalid date
 			"amount": 8
 		}
 	*/
